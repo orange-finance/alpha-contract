7fe54baaa46c92ca8126cc7c7150fb2a0af587cf
shinobu1729 <sino1729defi@gmail.com>
2023-05-23 14:56:28 +0700

    read whitlist from github, remove dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,3 @@
-import * as dotenv from "dotenv";
-
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-chai-matchers";
--- scripts/tx/generateMerkleRoot.ts
+++ scripts/tx/generateMerkleRoot.ts
@@ -1,9 +1,21 @@
-import { ethers } from "hardhat";
-import { Deploy } from "../common";
+import { ethers } from "ethers";
 import { getAddresses } from "../addresses";
-import { OrangeAlphaParameters } from "../../typechain-types/index";
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require("keccak256");
+
+const url =
+  "https://raw.githubusercontent.com/orange-finance/whitelist/main/WL.json";
+
+//get Json from Github
+async function fetchGitHubJson(url: string): Promise<any> {
+  const response = await fetch(url); //use node version ^18.0
+
+  if (!response.ok) {
+    throw new Error("Request Failed. Reason:" + response.status);
+  }
+
+  return await response.json();
+}
 
 async function hashed(target: any[]) {
   return target.map(({ address }) => {
@@ -11,32 +23,10 @@ async function hashed(target: any[]) {
   });
 }
 
-const list = [
-  {
-    address: "",
-  },
-  {
-    address: "",
-  },
-  {
-    address: "",
-  },
-  {
-    address: "",
-  },
-  {
-    address: "",
-  },
-  {
-    address: "",
-  },
-  {
-    address: "",
-  },
-];
-
 async function main() {
   const a = getAddresses()!;
+  const list = await fetchGitHubJson(url);
+  console.log(list);
 
   const leaves = await hashed(list);
   const tree = await new MerkleTree(leaves, keccak256, { sort: true });
