1e7f4d80dff9b493937ee970f328a509d9659d4f
0xnanika <0xnanika@orangefinance.io>
2023-05-26 16:16:22 +0900

    merkle test

---
+++ test/OrangeValidationCheckerTest.test.js
@@ -0,0 +1,66 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { BigNumber } = require("ethers");
+const { MerkleTree } = require("merkletreejs");
+const keccak256 = require("keccak256");
+
+async function hashed(target) {
+  return target.map(({ address }) => {
+    return ethers.utils.solidityKeccak256(["address"], [address]);
+  });
+}
+
+describe("OrangeValidationCheckerTest", function () {
+  before(async () => {
+    //import
+    [alice, bob, carol] = await ethers.getSigners();
+    const OrangeParametersV1 = await ethers.getContractFactory(
+      "OrangeParametersV1"
+    );
+    params = await OrangeParametersV1.deploy();
+    const OrangeValidationCheckerMock = await ethers.getContractFactory(
+      "OrangeValidationCheckerMock"
+    );
+    mock = await OrangeValidationCheckerMock.deploy(params.address);
+
+    // console.log(bob.address);
+    // console.log(bob.address.toLowerCase());
+    let list = [
+      {
+        address: alice.address,
+      },
+      {
+        address: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8", //bob.address (lowerCase)
+      },
+      {
+        address: carol.address.toLowerCase(), //lowerCase
+      },
+    ];
+    const leaves = await hashed(list);
+    const tree = await new MerkleTree(leaves, keccak256, { sort: true });
+    const root = await tree.getHexRoot();
+
+    params.setMerkleRoot(root);
+
+    const leaf0 = leaves[0];
+    proof0 = await tree.getHexProof(leaf0);
+    const leaf1 = leaves[1];
+    proof1 = await tree.getHexProof(leaf1);
+    const leaf2 = leaves[2];
+    proof2 = await tree.getHexProof(leaf2);
+  });
+
+  describe("exec", function () {
+    it("success", async () => {
+      await mock.connect(alice).exec(proof0);
+      await mock.connect(bob).exec(proof1);
+      await mock.connect(carol).exec(proof2);
+    });
+    it("fail", async () => {
+      await expect(mock.connect(alice).exec(proof1)).to.revertedWith("201");
+    });
+    it("fail2", async () => {
+      await expect(mock.connect(carol).exec(proof0)).to.revertedWith("201");
+    });
+  });
+});
