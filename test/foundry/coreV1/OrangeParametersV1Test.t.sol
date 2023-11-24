c9bf455e7be1f2150f5784c9c0c0e5a8d6dafb4d
maguro <maguro.abs@gmail.com>
2023-11-24 11:56:13 +0900

    feat: OrangePrametersV1 - added setter events

--- test/foundry/coreV1/OrangeParametersV1Test.t.sol
+++ test/foundry/coreV1/OrangeParametersV1Test.t.sol
@@ -8,6 +8,16 @@ import {OrangeParametersV1, ErrorsV1} from "../../../contracts/coreV1/OrangePara
 contract OrangeParametersV1Test is BaseTest {
     OrangeParametersV1 params;
 
+    event SetSlippage(uint16 slippageBPS, uint24 tickSlippageBPS);
+    event SetTwapSlippageInterval(uint32 twapSlippageInterval);
+    event SetMaxLtv(uint32 maxLtv);
+    event SetAllowlistEnabled(bool allowlistEnabled);
+    event SetMerkleRoot(bytes32 merkleRoot);
+    event SetDepositCap(uint256 depositCap);
+    event SetMinDepositAmount(uint256 minDepositAmount);
+    event SetHelper(address helper);
+    event SetStrategyImpl(address strategyImpl);
+
     function setUp() public {
         //deploy parameters
         params = new OrangeParametersV1();
@@ -52,31 +62,49 @@ contract OrangeParametersV1Test is BaseTest {
     }
 
     function test_Success() public {
+        vm.expectEmit(true, true, true, true);
+        emit SetSlippage(1, 1);
         params.setSlippage(1, 1);
         assertEq(params.slippageBPS(), 1);
         assertEq(params.tickSlippageBPS(), 1);
 
+        vm.expectEmit(true, true, true, true);
+        emit SetTwapSlippageInterval(1);
         params.setTwapSlippageInterval(1);
         assertEq(params.twapSlippageInterval(), 1);
 
+        vm.expectEmit(true, true, true, true);
+        emit SetMaxLtv(1);
         params.setMaxLtv(1);
         assertEq(params.maxLtv(), 1);
 
+        vm.expectEmit(true, true, true, true);
+        emit SetAllowlistEnabled(true);
         params.setAllowlistEnabled(true);
         assertEq(params.allowlistEnabled(), true);
 
+        vm.expectEmit(true, true, true, true);
+        emit SetMerkleRoot(0x0);
         params.setMerkleRoot(0x0);
         assertEq(params.merkleRoot(), 0x0);
 
+        vm.expectEmit(true, true, true, true);
+        emit SetDepositCap(1);
         params.setDepositCap(1);
         assertEq(params.depositCap(), 1);
 
+        vm.expectEmit(true, true, true, true);
+        emit SetMinDepositAmount(1);
         params.setMinDepositAmount(1);
         assertEq(params.minDepositAmount(), 1);
 
+        vm.expectEmit(true, true, true, true);
+        emit SetHelper(alice);
         params.setHelper(alice);
         assertEq(params.helper(), alice);
 
+        vm.expectEmit(true, true, true, true);
+        emit SetStrategyImpl(address(this));
         params.setStrategyImpl(address(this));
         assertEq(params.strategyImpl(), address(this));
     }
